2139aceabf31d89c3f8acf592b0f4f9012cbec1e
Alysa <alysa@flowmind.dev>
2026-03-19 19:05:19 -0500

    fix: sidebar JSX unclosed div causing parse error

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -158,7 +158,6 @@ export function Sidebar({ className }: SidebarProps) {
 
       {/* Import dialog */}
       <ExternalImportDialog open={importOpen} onOpenChange={setImportOpen} />
-      </div>
     </motion.nav>
   );
 }
